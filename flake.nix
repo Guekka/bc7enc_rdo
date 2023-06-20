2ebcab436faace4e684f52da23e3e465e32874c7
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-06-20 16:07:20 +0200

    build: add clang support

--- flake.nix
+++ flake.nix
@@ -1,21 +1,27 @@
 {
   description = "virtual environments";
 
-  inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, flake-utils, devshell, nixpkgs }:
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+  }:
     flake-utils.lib.eachDefaultSystem (system: {
-      devShell =
-        let
-          pkgs = import nixpkgs {
-            inherit system;
+      packages.default = let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        pkgs.stdenv.mkDerivation {
+          pname = "bc7enc";
+          version = "1.0.0";
 
-            overlays = [ devshell.overlays.default ];
-          };
-        in
-        pkgs.devshell.mkShell {
-          packages = with pkgs; [cmake gcc ninja];
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [cmake llvmPackages_16.clang ninja];
+	  buildInputs = with pkgs; [ llvmPackages_16.openmp ];
         };
     });
 }
